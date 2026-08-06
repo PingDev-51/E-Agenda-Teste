2a3386362e77868625f36a94914fb5c6c5389d92
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-06 19:54:38 -0300

    adiciona teste de telefone duplicado

--- tests/eAgenda.Testes.Unidade/Modulos/ModuloContato/ServicoContatoTests.cs
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloContato/ServicoContatoTests.cs
@@ -36,4 +36,32 @@ public sealed class ServicoContatoTests
 
         repositorioContato.Verify(r => r.Cadastrar(It.IsAny<Contato>()), Times.Never);
     }
+
+    [TestMethod]
+    public void Cadastrar_TelefoneDuplicado_RetornaFalha()
+    {
+        //Arrange
+        Mock<IRepositorioContato> repositorioContato = new Mock<IRepositorioContato>();
+        Mock<IRepositorioCompromisso> repositorioCompromisso = new Mock<IRepositorioCompromisso>();
+
+        repositorioContato
+            .Setup(r => r.SelecionarTodos())
+            .Returns([new Contato("Kauan", "kauazindelas145@gmail.com", "(49) 98883-1234", "dev", "amvn")]);
+
+        ServicoContato servicoContato = new ServicoContato(
+            repositorioContato.Object,
+            repositorioCompromisso.Object
+        );
+
+        //Act
+        Result resultado = servicoContato
+            .Cadastrar(new CadastrarContatoDto("neymar", "neymartest321@gmail.com", "(49) 98883-1234", "jogador", "santos"));
+
+        //Assert
+        Assert.IsTrue(resultado.IsFailed);
+        Assert.AreEqual("Telefone", resultado.Errors.Single().Metadata["Campo"]);
+        Assert.Contains("Já existe", resultado.Errors.Single().Message);
+
+        repositorioContato.Verify(r => r.Cadastrar(It.IsAny<Contato>()), Times.Never);
+    }
 }

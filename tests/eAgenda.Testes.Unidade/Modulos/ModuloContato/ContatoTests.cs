972caf6fb387e990a8ad6a66575e6d7b2b5ab4cf
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-06 17:53:08 -0300

    add: contato tests

---
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloContato/ContatoTests.cs
@@ -0,0 +1,76 @@
+using eAgenda.Dominio.Modulos.ModuloContato;
+
+namespace eAgenda.Testes.Unidade.Modulos.ModuloContato;
+
+[TestClass]
+public sealed class ContatoTests
+{
+    [TestMethod]
+    public void CadastrarContato_ComTodosOsCamposPreenchidos()
+    {
+        //Arange
+        Contato contato = new Contato("Kauan", "kauazindelas145@gmail.com", "(49) 98883-1234", "dev", "amvn");
+
+        //Act
+        List<string> erros = contato.Validar();
+
+        //Assert
+        Assert.HasCount(0, erros);
+    }
+
+    [TestMethod]
+    public void CadastrarContato_ComOsCamposObrigatoriosPreenchidos()
+    {
+        //Arange
+        Contato contato = new Contato("Kauan", "kauazindelas145@gmail.com", "(49) 98883-1234", null, string.Empty);
+
+        //Act
+        List<string> erros = contato.Validar();
+
+        //Assert
+        Assert.HasCount(0, erros);
+    }
+
+
+    [TestMethod]
+    public void CadastrarContato_ComOsCamposObrigatoriosEmBranco()
+    {
+        //Arange
+        Contato contato = new Contato(string.Empty, string.Empty, string.Empty, null, string.Empty);
+
+        //Act
+        List<string> erros = contato.Validar();
+
+        //Assert
+        Assert.HasCount(3, erros);
+        Assert.AreEqual(
+            "O campo \"Nome\" deve conter entre 2 e 100 caracteres.",
+            erros[0]
+        );
+        Assert.AreEqual(
+            "O campo \"E-mail\" deve conter um endereço de e-mail válido.",
+            erros[1]
+        );
+        Assert.AreEqual(
+            "O campo \"Telefone\" deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX.",
+            erros[2]
+        );
+    }
+
+    [TestMethod]
+    public void CadastrarContato_ComONomeAbaixoDoMinimo()
+    {
+        //Arange
+       Contato contato = new Contato("K", "kauazindelas145@gmail.com", "(49) 98883-1234", null, string.Empty);
+
+        //Act
+        List<string> erros = contato.Validar();
+
+        //Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Nome\" deve conter entre 2 e 100 caracteres.",
+            erros.First()
+        );
+    }
+}

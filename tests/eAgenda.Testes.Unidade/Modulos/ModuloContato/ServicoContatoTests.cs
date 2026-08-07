d70b42390c95caba9a585715210382c78b669588
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-07 17:19:00 -0300

    add: SelecionarTodos_DeveRetornarTodosOsContatos

--- tests/eAgenda.Testes.Unidade/Modulos/ModuloContato/ServicoContatoTests.cs
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloContato/ServicoContatoTests.cs
@@ -155,4 +155,117 @@ public sealed class ServicoContatoTests
 
         repositorioContato.Verify(r => r.Editar(It.IsAny<Guid>(), It.IsAny<Contato>()), Times.Never);
     }
+
+    [TestMethod]
+    public void Editar_Contato_MantendoEmail_E_Telefone()
+    {
+        // Arrange
+        Guid contatoId = Guid.CreateVersion7();
+
+        Mock<IRepositorioContato> repositorioContato = new();
+        Mock<IRepositorioCompromisso> repositorioCompromisso = new();
+
+        Contato contatoExistente = new(
+            "Neymar",
+            "neymar@gmail.com",
+            "(49) 98883-1234",
+            "Jogador",
+            "Santos"
+        )
+        {
+            Id = contatoId
+        };
+
+        repositorioContato.Setup(r => r.SelecionarTodos()).Returns([contatoExistente]);
+
+        repositorioContato
+            .Setup(r => r.Editar(contatoId, It.IsAny<Contato>()))
+            .Returns(true);
+
+        ServicoContato servicoContato = new(
+            repositorioContato.Object,
+            repositorioCompromisso.Object
+        );
+
+        // Act
+        Result resultado = servicoContato.Editar(
+            new EditarContatoDto(
+                contatoId,
+                "Vini jr",
+                "neymar@gmail.com",
+                "(49) 98883-1234",
+                "Jogador",
+                "Real Madrid"
+            )
+        );
+
+        // Assert
+        resultado.IsSuccess.Should().BeTrue();
+
+        repositorioContato.Verify(
+            r => r.Editar(contatoId, It.IsAny<Contato>()),
+            Times.Once
+        );
+    }
+
+    [TestMethod]
+    public void SelecionarTodos_DeveRetornarTodosOsContatos()
+    {
+        // Arrange
+        Mock<IRepositorioContato> repositorioContato = new();
+        Mock<IRepositorioCompromisso> repositorioCompromisso = new();
+
+        Contato contato1 = new(
+            "Kauan S.",
+            "kauans@gmail.com",
+            "(49) 99999-9999",
+            "Programador",
+            "Empresa Fantasma"
+        );
+
+        Contato contato2 = new(
+            "Pedrinho",
+            "pedrinhoteste@gmail.com",
+            "(49) 98888-8888",
+            "Designer",
+            "Empresa Fantasma"
+        );
+
+        repositorioContato
+            .Setup(r => r.SelecionarTodos())
+            .Returns([contato1, contato2]);
+
+        ServicoContato servicoContato = new(
+            repositorioContato.Object,
+            repositorioCompromisso.Object
+        );
+
+        // Act
+        List<ListarContatosDto> resultado = servicoContato.SelecionarTodos();
+
+        // Assert
+        resultado.Should().HaveCount(2);
+
+        resultado.Should().BeEquivalentTo(
+        new ListarContatosDto(
+         contato1.Id,
+         contato1.Nome,
+         contato1.Email,
+         contato1.Telefone,
+         contato1.Cargo,
+         contato1.Empresa
+        ),
+        new ListarContatosDto(
+         contato2.Id,
+         contato2.Nome,
+         contato2.Email,
+         contato2.Telefone,
+         contato2.Cargo,
+         contato2.Empresa
+     )
+    );
+        repositorioContato.Verify(
+            r => r.SelecionarTodos(), Times.Once
+        );
+    }
 }

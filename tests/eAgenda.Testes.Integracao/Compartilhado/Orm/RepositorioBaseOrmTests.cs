fb478f4e721eb15db2d5253a94a106e4e53a9da9
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-09 00:19:18 -0300

    add & fix: resolve base orm, e adiciona teste de cadastro infra

--- tests/eAgenda.Testes.Integracao/Compartilhado/Orm/RepositorioBaseOrmTests.cs
+++ tests/eAgenda.Testes.Integracao/Compartilhado/Orm/RepositorioBaseOrmTests.cs
@@ -1,18 +1,16 @@
-
-using Microsoft.EntityFrameworkCore;
 using FizzWare.NBuilder;
-using eAgenda.Infra.Modulos.ModuloCategoria;
-using eAgenda.Infra.Modulos.ModuloCompromisso;
-using eAgenda.Infra.Modulos.ModuloContato;
-using eAgenda.Infra.Modulos.ModuloDespesa;
-using eAgenda.Infra.Modulos.ModuloTarefa;
-using eAgenda.Infra.Compartilhado.Orm;
 using eAgenda.Dominio.Modulos.ModuloCategoria;
 using eAgenda.Dominio.Modulos.ModuloCompromisso;
 using eAgenda.Dominio.Modulos.ModuloContato;
 using eAgenda.Dominio.Modulos.ModuloDespesa;
 using eAgenda.Dominio.Modulos.ModuloTarefa;
-
+using eAgenda.Infra.Compartilhado.Orm;
+using eAgenda.Infra.Modulos.ModuloCategoria;
+using eAgenda.Infra.Modulos.ModuloCompromisso;
+using eAgenda.Infra.Modulos.ModuloContato;
+using eAgenda.Infra.Modulos.ModuloDespesa;
+using eAgenda.Infra.Modulos.ModuloTarefa;
+using Microsoft.EntityFrameworkCore;
 
 namespace eAgenda.Testes.Integracao.Compartilhado.Orm;
 
@@ -28,62 +26,82 @@ public abstract class RepositorioBaseEmOrmTests
     [TestInitialize]
     public void InicializarContexto()
     {
+        var options = new DbContextOptionsBuilder<EAgendaDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        dbContext = new EAgendaDbContext(options);
 
         // Categoria
         repositorioCategoria = new RepositorioCategoriaEmOrm(dbContext);
 
-        BuilderSetup.SetCreatePersistenceMethod<Categoria>(repositorioCategoria.Cadastrar);
-        BuilderSetup.SetCreatePersistenceMethod<IList<Categoria>>((categorias) =>
-        {
-            foreach (Categoria d in categorias)
-                repositorioCategoria.Cadastrar(d);
-        });
+        BuilderSetup.SetCreatePersistenceMethod<Categoria>(
+            repositorioCategoria.Cadastrar);
+
+        BuilderSetup.SetCreatePersistenceMethod<IList<Categoria>>(
+            categorias =>
+            {
+                foreach (Categoria categoria in categorias)
+                    repositorioCategoria.Cadastrar(categoria);
+            });
 
         // Compromisso
         repositorioCompromisso = new RepositorioCompromissoEmOrm(dbContext);
 
-        BuilderSetup.SetCreatePersistenceMethod<Compromisso>(repositorioCompromisso.Cadastrar);
-        BuilderSetup.SetCreatePersistenceMethod<IList<Compromisso>>((compromisso) =>
-        {
-            foreach (Compromisso c in compromisso)
-                repositorioCompromisso.Cadastrar(c);
-        });
+        BuilderSetup.SetCreatePersistenceMethod<Compromisso>(
+            repositorioCompromisso.Cadastrar);
 
-        // Questao
+        BuilderSetup.SetCreatePersistenceMethod<IList<Compromisso>>(
+            compromissos =>
+            {
+                foreach (Compromisso compromisso in compromissos)
+                    repositorioCompromisso.Cadastrar(compromisso);
+            });
+
+        // Contato
         repositorioContato = new RepositorioContatoEmOrm(dbContext);
 
-        BuilderSetup.SetCreatePersistenceMethod<Contato>(repositorioContato.Cadastrar);
-        BuilderSetup.SetCreatePersistenceMethod<IList<Contato>>((contatos) =>
-        {
-            foreach (Contato c in contatos)
-                repositorioContato.Cadastrar(c);
-        });
+        BuilderSetup.SetCreatePersistenceMethod<Contato>(
+            repositorioContato.Cadastrar);
 
-        // Prova
+        BuilderSetup.SetCreatePersistenceMethod<IList<Contato>>(
+            contatos =>
+            {
+                foreach (Contato contato in contatos)
+                    repositorioContato.Cadastrar(contato);
+            });
+
+        // Despesa
         repositorioDespesa = new RepositorioDespesaEmOrm(dbContext);
 
-        BuilderSetup.SetCreatePersistenceMethod<Despesa>(repositorioDespesa.Cadastrar);
-        BuilderSetup.SetCreatePersistenceMethod<IList<Despesa>>((despesas) =>
-        {
-            foreach (Despesa d in despesas)
-                repositorioDespesa.Cadastrar(d);
-        });
+        BuilderSetup.SetCreatePersistenceMethod<Despesa>(
+            repositorioDespesa.Cadastrar);
 
+        BuilderSetup.SetCreatePersistenceMethod<IList<Despesa>>(
+            despesas =>
+            {
+                foreach (Despesa despesa in despesas)
+                    repositorioDespesa.Cadastrar(despesa);
+            });
 
         // Tarefa
         repositorioTarefa = new RepositorioTarefaEmOrm(dbContext);
 
-        BuilderSetup.SetCreatePersistenceMethod<Tarefa>(repositorioTarefa.Cadastrar);
-        BuilderSetup.SetCreatePersistenceMethod<IList<Tarefa>>((tarefas) =>
-        {
-            foreach (Tarefa t in tarefas)
-                repositorioTarefa.Cadastrar(t);
-        });
+        BuilderSetup.SetCreatePersistenceMethod<Tarefa>(
+            repositorioTarefa.Cadastrar);
+
+        BuilderSetup.SetCreatePersistenceMethod<IList<Tarefa>>(
+            tarefas =>
+            {
+                foreach (Tarefa tarefa in tarefas)
+                    repositorioTarefa.Cadastrar(tarefa);
+            });
     }
 
     [TestCleanup]
     public void DescartarContexto()
     {
-        dbContext.Dispose();
+        dbContext?.Dispose();
     }
 }
+
---
+++ tests/eAgenda.Testes.Integracao/Modulos/ModuloContato/RepositorioContatoEmOrmTests.cs
@@ -0,0 +1,41 @@
+using eAgenda.Dominio.Modulos.ModuloContato;
+using eAgenda.Testes.Integracao.Compartilhado.Orm;
+using FizzWare.NBuilder;
+
+namespace eAgenda.Testes.Integracao.Modulos.ModuloContato;
+
+[TestClass]
+public sealed class RepositorioContatoEmOrmTests(): RepositorioBaseEmOrmTests
+{
+    [TestMethod]
+    public void CadastrarESelecionarPorId_CarregaRegistro()
+    {
+        // Arranjo
+        Contato contato = Builder<Contato>
+            .CreateNew()
+            .With(c => c.Nome = "Nome1")
+            .With(c => c.Telefone = "(00) 00000-0000")
+            .With(c => c.Email = "test@gmail.com")
+            .With(c => c.Cargo = "testador")
+            .With(c => c.Empresa = "Testes")
+
+            .Build();
+
+        // Ação
+        repositorioContato.Cadastrar(contato);
+        dbContext.ChangeTracker.Clear();
+
+        Contato? contatoSelecionado = repositorioContato.SelecionarPorId(contato.Id);
+
+        // Asserção
+        Assert.IsNotNull(contatoSelecionado);
+        Assert.AreEqual(contato.Id, contatoSelecionado.Id);
+        Assert.AreEqual("Nome1", contatoSelecionado.Nome);
+        Assert.AreEqual("(00) 00000-0000", contatoSelecionado.Telefone);
+        Assert.AreEqual("test@gmail.com", contatoSelecionado.Email);
+        Assert.AreEqual("testador", contatoSelecionado.Cargo);
+        Assert.AreEqual("Testes", contatoSelecionado.Empresa);
+    }
+}
+
+

14575449c39a19f691de068d9af607b4a8b430b1
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-09 07:21:43 -0300

    refatora testes e deixa coerente com a planilha

--- tests/eAgenda.Testes.Unidade/Modulos/ModuloTarefa/TarefaTests.cs
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloTarefa/TarefaTests.cs
@@ -1,3 +1,4 @@
+using eAgenda.Aplicacao.Modulos.ModuloTarefa;
 using eAgenda.Dominio.Modulos.ModuloTarefa;
 
 namespace eAgenda.Testes.Unidade.Modulos.ModuloTarefa;
@@ -5,17 +6,28 @@ namespace eAgenda.Testes.Unidade.Modulos.ModuloTarefa;
 [TestClass]
 public sealed class TarefaTests()
 {
+
+
     [TestMethod]
     public void CadastrarTarefa_ComTodosOsCamposPreenchidos()
     {
-        //Arange
-        Tarefa tarefa = new Tarefa("Testar", PrioridadeTarefa.Baixa);
+        // Arrange
+        Tarefa tarefa = new Tarefa(
+            "Testar",
+            PrioridadeTarefa.Alta
+        );
 
-        //Act
+        // Act
         List<string> erros = tarefa.Validar();
 
-        //Assert
+        // Assert
         Assert.HasCount(0, erros);
+        Assert.AreEqual("Testar", tarefa.Titulo);
+        Assert.AreEqual(PrioridadeTarefa.Alta, tarefa.Prioridade);
+        Assert.AreEqual(DateTime.Today, tarefa.DataCriacao);
+        Assert.IsNull(tarefa.DataConclusao);
+        Assert.IsFalse(tarefa.Concluida);
+        Assert.AreEqual(0, tarefa.PercentualConcluido);
     }
 
 
@@ -24,19 +36,37 @@ public sealed class TarefaTests()
     {
         // Arrange
         Tarefa tarefa = new Tarefa(
-            "Testar",
+            "Test",
             PrioridadeTarefa.Normal
         );
+
+        // Act
+        List<string> erros = tarefa.Validar();
 
         // Assert
         Assert.IsFalse(tarefa.Concluida);
         Assert.AreEqual(0, tarefa.PercentualConcluido);
         Assert.AreEqual(DateTime.Today, tarefa.DataCriacao);
         Assert.IsNull(tarefa.DataConclusao);
+        Assert.HasCount(0, erros);
     }
 
 
+    [TestMethod]
+    public void CadastrarTarefa_SemItens()
+    {
+        // Arrange
+        Tarefa tarefa = new Tarefa(
+            "Testar",
+            PrioridadeTarefa.Normal
+        );
 
+        //Act
+        List<string> erros = tarefa.Validar();
 
+        // Assert
+        Assert.AreEqual(0, tarefa.PercentualConcluido);
+        Assert.HasCount(0, erros);
+    }
 
 }

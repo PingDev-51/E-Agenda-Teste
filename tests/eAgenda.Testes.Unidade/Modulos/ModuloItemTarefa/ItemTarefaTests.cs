2942da6b8773d19cfd687cd8a418718fcb258b7f
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-11 19:01:44 -0300

    refactor: renomeia método de teste para CadastrarItem e adiciona validações de título

--- tests/eAgenda.Testes.Unidade/Modulos/ModuloItemTarefa/ItemTarefaTests.cs
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloItemTarefa/ItemTarefaTests.cs
@@ -6,7 +6,7 @@ namespace eAgenda.Testes.Unidade.Modulos.ModuloItemTarefa;
 public sealed class ItemTarefaTests
 {
     [TestMethod]
-    public void CadastrarTarefa_ComTodosOsCamposPreenchidos()
+    public void CadastrarItem_ComTodosOsCamposPreenchidos()
     {
         // Arrange
         ItemTarefa itemTarefa = new ItemTarefa(
@@ -19,4 +19,64 @@ public sealed class ItemTarefaTests
         // Assert
         Assert.HasCount(0, erros);
     }
+
+    [TestMethod]
+    public void CadastrarItem_SemTitulo()
+    {
+        // Arrange
+        ItemTarefa itemTarefa = new ItemTarefa(
+            string.Empty
+        );
+
+        // Act
+        List<string> erros = itemTarefa.Validar();
+
+        // Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+           "O campo \"Título\" deve conter entre 2 e 100 caracteres.",
+           erros.First()
+       );
+    }
+
+    [TestMethod]
+    public void CadastrarItem_ComTituloAbaixoDoMinimo()
+    {
+        // Arrange
+        ItemTarefa itemTarefa = new ItemTarefa(
+            "T"
+        );
+
+        // Act
+        List<string> erros = itemTarefa.Validar();
+
+        // Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+           "O campo \"Título\" deve conter entre 2 e 100 caracteres.",
+           erros.First()
+       );
+    }
+
+    [TestMethod]
+    public void CadastrarItem_ComTituloAcumaMinimo()
+    {
+        // Arrange
+        string tarefa = new string('t', 101);
+
+        ItemTarefa itemTarefa = new ItemTarefa(
+            tarefa
+        );
+
+        // Act
+        List<string> erros = itemTarefa.Validar();
+
+        // Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+           "O campo \"Título\" deve conter entre 2 e 100 caracteres.",
+           erros.First()
+       );
+    }
+
 }
